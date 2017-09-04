8af0f82adbd49c7417af6381df04687358f8e8bf
beautifuldonkey <beautifuldonkey949@gmail.com>
2017-09-04 02:07:54 -0700

    BFSDataMain.java -extending jetty webapp context rather than servlet --allows angular & html to load when host accessed

--- src/main/java/BFSDataMain.java
+++ src/main/java/BFSDataMain.java
@@ -1,38 +1,39 @@
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.DefaultServlet;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.resource.Resource;
-import java.net.URI;
-import java.net.URL;
+import org.eclipse.jetty.webapp.WebAppContext;
 
+/**
+ * Primary application class for BFS Data Server
+ */
 public class BFSDataMain {
-    public static void main(String[] args){
-        try{
-            Server bfsServer = new Server(7070);
-
-            ClassLoader cl = BFSDataMain.class.getClassLoader();
-
-            URL url = cl.getResource("index.html");
-            if(url == null){
-                throw new RuntimeException("Unable to find resources.");
-            }
-
-            URI webRootUri = url.toURI().resolve("./").normalize();
-            System.out.println("WebRoot is "+webRootUri);
-
-            ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-            handler.setContextPath("/main");
-            handler.setBaseResource(Resource.newResource(webRootUri));
-            bfsServer.setHandler(handler);
-
-            ServletHolder holderPwd = new ServletHolder("default", DefaultServlet.class);
-            holderPwd.setInitParameter("dirAllowed","true");
-            handler.addServlet(holderPwd,"/");
-
-            bfsServer.start();
-        }catch (Exception ex){
-            System.out.println("Server Error: "+ex.getMessage());
-        }
+  /**
+   * Primary method extended by class
+   * @param args
+   */
+  public static void main(String[] args){
+    try {
+      bootServer();
+    }catch (Exception ex){
+      System.out.println("System Error:"+ex.getMessage());
     }
+
+  }
+
+  private static void bootServer() throws Exception{
+    try{
+      Server bfsServer = new Server(7070);
+      String webappDir = "src/main/webapp/";
+
+      WebAppContext webapp = new WebAppContext();
+      webapp.setContextPath("/");
+      webapp.setDescriptor(webappDir+"/WEB-INF/web.xml");
+      webapp.setResourceBase(webappDir);
+
+      bfsServer.setHandler(webapp);
+      bfsServer.start();
+      bfsServer.join();
+    }catch (InterruptedException intEx){
+      System.out.println("Interrupted: "+intEx.getMessage());
+    }
+  }
+
 }
