359b903b7df24ea7b8badac2d141f2b01ea32cbf
beautifuldonkey <beautifuldonkey949@gmail.com>
2017-10-26 19:30:29 -0700

    BFSDataMain.java -updating db interface class name -adding db testing functions

--- src/main/java/BFSDataMain.java
+++ src/main/java/BFSDataMain.java
@@ -1,13 +1,15 @@
-import database.DbInit;
+import database.MySqlDatabaseService;
+import database.TABLE_Test;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import java.util.List;
 
 /**
  * Primary application class for BFS Data Server
  */
 public class BFSDataMain {
 
-  private static DbInit db;
+  private static MySqlDatabaseService db;
   private static Server bfsServer;
   private static int serverPort = 7070;
   private static String webappDir = "src/main/webapp/";
@@ -26,10 +28,6 @@ public class BFSDataMain {
       bootDbInterface();
       System.out.println("DB Interface is set up!");
       System.out.println("connection status: " + db.isConnectionActive());
-
-      // validating DB connection
-      db.getTestRecords();
-      System.out.println("Retrieved test records:");
     }catch (Exception ex){
       System.out.println("System Exception:"+ex.getMessage());
     }
@@ -40,7 +38,7 @@ public class BFSDataMain {
   }
 
   private static void bootDbInterface(){
-    db = new DbInit();
+    db = new MySqlDatabaseService();
   }
 
   private static void bootServer() throws Exception {
@@ -56,6 +54,19 @@ public class BFSDataMain {
     }catch (InterruptedException intEx){
       System.out.println("Interrupted: "+intEx.getMessage());
     }
+  }
+
+  private void insertTestRecord(){
+    TABLE_Test test = new TABLE_Test();
+    test.setCol1("bleh2");
+    test.setId(2);
+    db.insertRecord(test);
+    System.out.println("Record inserted!");
+  }
+
+  private void getTestRecords(){
+    List<TABLE_Test> list = db.getTestRecords();
+    System.out.println("Retrieved test records:");
   }
 
 }
