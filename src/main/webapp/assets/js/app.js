cc35aba020922c1fbcf28008c5c10e81bf558060
beautifuldonkey <beautifudlonkey949@gmail.com>
2018-01-22 19:30:52 -0800

    app.js - uncommenting 'greeting' directory

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -1,21 +1,19 @@
 var app = angular.module('app',['ngRoute']);
 
-// app.directive('greeting',function () {
-//   return {
-//     restrict: 'C',
-//     link: function () {
-//       alert('Hey Yo!');
-//     }
-//   }
-// });
+app.directive('greeting',function () {
+  return {
+    restrict: 'C',
+    link: function () {
+      alert('Hey Yo!');
+    }
+  }
+});
 
 app.factory('bfsDataService', function ($http) {
   return {
     checkStatus: function () {
-      debugger;
       return $http.get('/main/db/checkStatus')
           .then(function (value) {
-            debugger;
             return value.data;
           });
     }
@@ -25,12 +23,10 @@ app.factory('bfsDataService', function ($http) {
 app.controller('bfsDataController', function ($scope, bfsDataService) {
   var self = this;
   bfsDataService.checkStatus().then(function (value) {
-    debugger;
     $scope.dbStatus = value;
   });
 
   self.getDbStatus = function () {
-    debugger;
     console.log('checking status');
     bfsDataService.checkStatus();
   }
