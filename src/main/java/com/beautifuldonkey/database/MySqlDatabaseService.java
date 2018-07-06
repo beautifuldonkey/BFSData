200eba2c82c6948b959208d503dea7e635300946
beautifuldonkey <beautifudlonkey949@gmail.com>
2018-07-05 20:54:24 -0700

    - moving a ton of things around so proj directory structure more closely reflects standard web project structure - implements spring + tomcat - will review later

---
+++ src/main/java/com/beautifuldonkey/database/MySqlDatabaseService.java
@@ -0,0 +1,51 @@
+package com.beautifuldonkey.database;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import java.sql.Connection;
+
+public class MySqlDatabaseService {
+
+  private static SessionFactory factory;
+  private static Session session;
+  private Connection dbConnection;
+  private String dbUn = "BFSData";
+  private String dbUrl = "BFSData@127.0.0.1:3306";
+//  private String dbUrl = "jdbc:mysql://127.0.0.1:3306/?user=BFSData";
+  private String dbPw = "bfsdata";
+
+  public MySqlDatabaseService(){
+    try {
+      if(factory == null || session == null){
+        factory = new Configuration().configure().buildSessionFactory();
+        session = factory.openSession();
+      }
+    }catch (Exception ex){
+      ex.printStackTrace();
+    }
+  }
+
+  public void setupSchema(){}
+
+  public boolean isConnectionActive(){
+    return session.isConnected();
+  }
+
+  public void purgeDb(){}
+
+  public void insertRecord(Object record){
+    session.beginTransaction();
+    session.merge(record);
+    session.getTransaction().commit();
+  }
+
+//  public List<TABLE_Test> getTestRecords(){
+//    return session.createNamedQuery("getRecords",TABLE_Test.class).list();
+//  }
+
+  public boolean checkNewRawData(){
+    return false;
+  }
+
+}
